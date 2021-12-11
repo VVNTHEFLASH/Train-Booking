0f45b0e820711ce5b898d6cab51b35223cc839d4
VVNTHEFLASH <vishnurockhunters@gmail.com>
2021-12-11 23:10:53 +0530

    Creating Types of Train Tickets

--- menu/menu.js
+++ menu/menu.js
@@ -1,9 +1,14 @@
+//Logout button
 var logout = document.getElementById('logout-btn');
 
 logout.onclick = () => {
     window.location.href = '../index.html';
 }
-
+//List Of Train Tickets
 var normal = document.getElementById('normal-train');
 var superfast = document.getElementById('superfast-train');
 var express = document.getElementById('express');
+
+normal.onclick = () => {
+    window.location.href = "../NormalTicket/normal.html";
+}
