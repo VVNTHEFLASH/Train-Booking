a0d85914dc74bccc0c7d4bd014c7247d32c0d4ba
VVNTHEFLASH <vishnurockhunters@gmail.com>
2022-01-25 12:51:52 +0530

    Modified Some Files

--- menu/menu.js
+++ menu/menu.js
@@ -7,14 +7,14 @@ logout.onclick = () => {
 //List Of Train Tickets
 var normal = document.getElementById('normal-train');
 var superfast = document.getElementById('superfast-train');
-var express = document.getElementById('express');
+var express = document.getElementById('express-train');
 
 normal.onclick = () => {
     window.location.href = "../NormalTicket/normal.html";
 }
 superfast.onclick = () => {
-    window.location.href = "../NormalTicket/normal.html";
+    window.location.href = "../SuperfastTicket/superfast.html";
 }
 express.onclick = () => {
-    window.location.href = "../NormalTicket/normal.html";
+    window.location.href = "../ExpressTicket/express.html";
 }
